2606f01d4313766bc889a13c7ed5d6a1da346437
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-11 13:51:46 +0000

    solution

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ Given an array of integers nums which is sorted in ascending order, and an integ
 
 You must write an algorithm with O(log n) runtime complexity.
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let pivot,left = 0
+    let right = nums.length - 1
+    while (left <= right){
+        pivot = Math.floor(left + (right - left) / 2)
+        if (target == nums[pivot]) return pivot
+        if (target < nums[pivot]){
+            right = pivot - 1
+        } 
+        else {
+            left = pivot + 1
+        }
+    }
+    return false
+};
+
+let nums = [-1,0,3,5,9,12]
+let target = 9
+search(nums, target)
